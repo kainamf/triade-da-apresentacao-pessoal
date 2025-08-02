4f5d74479986f43e9b8f5eaee7ccf8ea2c795b30
kainamf <kaina.marques@calindra.com.br>
2025-08-01 21:49:26 -0300

    refactor: Remove Services component from App and clean up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Hero from './components/Hero';
-import Services from './components/Services';
 import About from './components/About';
 import Values from './components/Values';
 import BrunaSpecialist from './components/BrunaSpecialist';
@@ -13,7 +12,6 @@ function App() {
     return (
         <div className="min-h-screen bg-white">
             <Hero />
-            <Services />
             <About />
             <Values />
             <BrunaSpecialist />
